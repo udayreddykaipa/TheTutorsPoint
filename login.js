c38527ed0176dd2b8e8f50c6beb8dda7c72e71d5
udayreddykaipa <udayreddy.kaipa@gmail.com>
2020-12-12 13:02:58 +0530

    work

---
+++ login.js
@@ -0,0 +1,7 @@
+console.log("sj")
+document.getElementById('TutorPoint').innerHTML = `<div class="card">
+<div class="card-head" style="text-align: center;">
+    <h2>TheTutorsPoint</h2>
+</div>
+<div id="firebaseui-auth-container"></div>
+</div>`;
